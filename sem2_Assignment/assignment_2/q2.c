37adb55365d2f07cd2bbc2bb584319cc15c11ab7
sudipto0315 <sudiptoray2003@gmail.com>
2023-04-27 12:50:34 +0530

    modified

--- sem2_Assignment/assignment_2/q2.c
+++ sem2_Assignment/assignment_2/q2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include<limits.h>
 int main() {
     int arr[50], n, i, largest, secLargest;
 
@@ -11,19 +11,35 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    largest = arr[0];
-    for (i = 0; i < n; i++) {
-        if (largest < arr[i]) {
-            largest = arr[i];
+    largest = INT_MIN;
+    secLargest = INT_MIN;
+    for ( i = 0; i < n; i++)
+    {
+        if (arr[i]>largest)
+        {
+            secLargest=largest;
+            largest=arr[i];
         }
+        else if (arr[i]>secLargest)
+        {
+            secLargest=arr[i];
+        }
+        
     }
+    
+    // largest = arr[0];
+    // for (i = 0; i < n; i++) {
+    //     if (largest < arr[i]) {
+    //         largest = arr[i];
+    //     }
+    // }
 
-    secLargest = arr[0];
-    for (i = 0; i < n; i++) {
-        if (arr[i] != largest && secLargest < arr[i]) {
-            secLargest = arr[i];
-        }
-    }
+    // secLargest = arr[0];
+    // for (i = 0; i < n; i++) {
+    //     if (arr[i] != largest && secLargest < arr[i]) {
+    //         secLargest = arr[i];
+    //     }
+    // }
 
     printf("The largest element in the array is: %d\n",largest);
     printf("The second largest element in the array is: %d\n", secLargest);
