5ea372d6ddc339c3a5612a74505c0c3bd2539165
sudipto0315 <sudiptoray2003@gmail.com>
2023-04-27 12:34:18 +0530

    completed q2 of singly linked list

--- sem2_Assignment/assignment_3/q2.c
+++ sem2_Assignment/assignment_3/q2.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 typedef struct Node_t Node_t;
 typedef struct Node_t *Node;
-typedef Node List;
+typedef Node head;
 
-struct Node_t {
+struct Node_t
+{
     int data;
     Node next;
 };
@@ -17,35 +18,89 @@ Node newNode(int data, Node next) {
     return node;
 }
 
-void addFirst(Node *pnode, int data) {
-    *pnode = newNode(data, *pnode);
+void insertFirst(Node *head, int data) {
+    *head = newNode(data, *head);
 }
 
-void printList(Node node) {
+int input_elements(Node *head){
+    int list_Len = 0,data=0;
+    printf("Enter the length of the Linked List: ");
+    scanf("%d",&list_Len);
+    for (int i = 1; i <= list_Len; i++)
+    {
+        printf("\nEnter the %d element: ",i);
+        scanf("%d",&data);
+        insertFirst(head, data);
+    }
+    
+}
+
+int printList(Node node) {
     while (node) {
         printf("%d -> ", node->data);
         node = node->next;
     }
     printf("NULL\n");
+    return 0 ;
 }
 
-int main()
+int size (Node head)
 {
-    int data=0;
-    Node list = NULL;
-    //user input for the linked list
-    int list_Len=0; 
-    printf("Enter the length of the linked list: ");
-    scanf("%d",&list_Len);
-    int inputElement =0;
-    printf("Enter the elements:\n");
-    for (int i = 1; i <= list_Len; i++)
-    {   
-        printf("Enter element %d : ",i);
-        scanf("%d", &inputElement);
-        addFirst(&list, inputElement);
+  int counter=0;
+
+  // as linked list will end when Node is Null
+  while (head != NULL)
+    {
+      head = head->next;
+      counter++;
     }
-    printf("The original Linked List is : "); printList(list);
-    
+ return counter;
+}
+
+int checkPalindrome (Node head, int counter)
+
+{
+    int i = 0, j;
+    Node front, rear;
+    while (i != counter / 2)
+    {
+        front = rear = head;
+        for (j = 0; j < i; j++)
+        {
+            front = front->next;
+        }
+        for (j = 0; j < counter - (i + 1); j++)
+        {
+            rear = rear->next;
+        }
+        if (front->data != rear->data)
+        {
+            return 0;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    Node head = NULL;
+    int counter,result;
+    input_elements(&head);
+    printf("The original Linked List is : "); printList(head);
+    counter = size(head);
+    result = checkPalindrome(head,counter);
+    if (result == 1)
+    {
+        printf("The linked list is a palindrome.\n");
+    }
+    else
+    {
+        printf("The linked list is not a palindrome.\n");
+    }
     return 0;
 }
