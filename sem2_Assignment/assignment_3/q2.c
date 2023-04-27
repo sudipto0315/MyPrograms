952a3875329b126ffa09ffa16d3b992ec1b42b47
sudipto0315 <sudiptoray2003@gmail.com>
2023-04-27 13:28:19 +0530

    q2 Done

--- sem2_Assignment/assignment_3/q2.c
+++ sem2_Assignment/assignment_3/q2.c
@@ -26,9 +26,10 @@ int input_elements(Node *head){
     int list_Len = 0,data=0;
     printf("Enter the length of the Linked List: ");
     scanf("%d",&list_Len);
+    printf("\n");
     for (int i = 1; i <= list_Len; i++)
     {
-        printf("\nEnter the %d element: ",i);
+        printf("Enter the %d element: ",i);
         scanf("%d",&data);
         insertFirst(head, data);
     }
@@ -86,21 +87,44 @@ int checkPalindrome (Node head, int counter)
     return 1;
 }
 
+int reverseLinkedList(Node *head){
+    Node prev = NULL;
+    Node current = *head;
+    Node next = NULL;
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
 int main(int argc, char const *argv[])
 {
     Node head = NULL;
     int counter,result;
     input_elements(&head);
-    printf("The original Linked List is : "); printList(head);
+    printf("\nThe original Linked List is : "); printList(head);
+
+    //Now checking weather a Linked List is Palindrome
+
     counter = size(head);
     result = checkPalindrome(head,counter);
     if (result == 1)
     {
-        printf("The linked list is a palindrome.\n");
+        printf("\nTh Llinked List is a Palindrome.\n");
     }
     else
     {
-        printf("The linked list is not a palindrome.\n");
+        printf("\nThe Linked List is NOT a Palindrome.\n");
     }
+
+    // reversing the Linked List
+
+    printf("\nNow reversing the Linked List:\n");
+    reverseLinkedList(&head);
+    printf("The Reversed Linked List is : "); printList(head);
     return 0;
 }
