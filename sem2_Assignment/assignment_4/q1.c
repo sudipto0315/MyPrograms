9104f5296ea3bdd4d16e88629a4a528f6595bedd
sudipto0315 <sudiptoray2003@gmail.com>
2023-05-07 19:44:54 +0530

    completed assignment 4

--- sem2_Assignment/assignment_4/q1.c
+++ sem2_Assignment/assignment_4/q1.c
@@ -1,91 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a node structure for the doubly linked list
-struct Node {
+typedef struct Node_t node;
+typedef node *Node;
+struct Node_t
+{
     int data;
-    struct Node* prev;
-    struct Node* next;
+    Node prev;
+    Node next;
 };
 
-// Function to create a new node
-struct Node* newNode(int data) {
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-    node->data = data;
-    node->prev = NULL;
-    node->next = NULL;
-    return node;
+Node newNode(int data)
+{
+    Node new = (Node)calloc(1, sizeof(node));
+    new->data = data;
+    return new;
 }
 
-// Function to insert a node at the end of the list
-void insertEnd(struct Node** head, struct Node** tail, struct Node* node) {
-    if (*head == NULL) {
-        *head = node;
-        *tail = node;
-    } else {
-        (*tail)->next = node;
-        node->prev = *tail;
-        *tail = node;
+void addLast(Node *head_add, int data)
+{
+    Node prev = NULL;
+    if (*(head_add) == NULL)
+    {
+        *head_add = newNode(data);
+    }
+
+    else
+    {
+        Node head = *head_add;
+        while (1)
+        {
+            prev = head;
+            if (head->next == NULL)
+                break;
+            head = head->next;
+        }
+        head->next = newNode(data);
+        head = head->next;
+        head->prev = prev;
     }
 }
 
-// Function to merge two sorted doubly linked lists
-struct Node* mergeLists(struct Node* head1, struct Node* head2) {
-    struct Node* mergedHead = NULL;
-    struct Node* mergedTail = NULL;
+void doubly_FlistReader(Node list)
+{
+    if (list == NULL)
+    {
+        printf("NULL");
+        return;
+    }
+    while (1)
+    {
 
-    while (head1 != NULL && head2 != NULL) {
-        if (head1->data < head2->data) {
-            insertEnd(&mergedHead, &mergedTail, newNode(head1->data));
-            head1 = head1->next;
-        } else {
-            insertEnd(&mergedHead, &mergedTail, newNode(head2->data));
-            head2 = head2->next;
+        printf("%d->", list->data);
+        if (list->next == NULL)
+        {
+            printf("NULL");
+            break;
+        }
+        list = list->next;
+    }
+}
+
+Node mergeIt(Node first, Node second)
+{
+    Node output = NULL;
+    int flag = 0;
+    while (first != NULL || second != NULL)
+    {
+        // doubly_FlistReader(output);
+        if (flag == 1)
+        {
+            while (first != NULL)
+            {
+                addLast(&output, first->data);
+                first = first->next;
+            }
+            return output;
+        }
+
+        if (flag == 2)
+        {
+            while (second != NULL)
+            {
+                addLast(&output, second->data);
+                second = second->next;
+            }
+            return output;
+        }
+
+        int data_first = first->data;
+        int data_second = second->data;
+        // printf("data_first:%d data_second: %d\n", data_first, data_second);
+        if (data_first < data_second)
+        {
+            addLast(&output, data_first);
+            if (first->next == NULL)
+            {
+                flag = 2;
+            }
+            first = first->next;
+        }
+
+        else
+        {
+            addLast(&output, data_second);
+            if (second->next == NULL)
+            {
+                flag = 1;
+            }
+            second = second->next;
         }
     }
-
-    while (head1 != NULL) {
-        insertEnd(&mergedHead, &mergedTail, newNode(head1->data));
-        head1 = head1->next;
-    }
-
-    while (head2 != NULL) {
-        insertEnd(&mergedHead, &mergedTail, newNode(head2->data));
-        head2 = head2->next;
-    }
-
-    return mergedHead;
+    return output;
 }
 
-// Function to print the doubly linked list
-void printList(struct Node* head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->next;
-    }
+void main()
+{
+    Node head1 = NULL, tail1 = NULL;
+    Node head2 = NULL, tail2 = NULL;
+    int n1;
+    printf("Enter the size of list 1: ");
+    scanf("%d", &n1);
     printf("\n");
-}
+    for (int i = 0; i < n1; i++)
+    {
+        int data = 0;
+        printf("Enter the %d element: ", i + 1);
+        scanf("%d", &data);
+        addLast(&head1, data);
+        printf("\n");
+    }
 
-// Main function to test the program
-int main() {
-    // Create two sorted doubly linked lists
-    struct Node* head1 = newNode(1);
-    head1->next = newNode(3);
-    head1->next->prev = head1;
-    head1->next->next = newNode(5);
-    head1->next->next->prev = head1->next;
+    int n2;
+    printf("Enter the size of list 2: ");
+    scanf("%d", &n2);
+    printf("\n");
+    for (int i = 0; i < n2; i++)
+    {
+        int data = 0;
+        printf("Enter the %d element: ", i + 1);
+        scanf("%d", &data);
+        addLast(&head2, data);
+        printf("\n");
+    }
 
-    struct Node* head2 = newNode(2);
-    head2->next = newNode(4);
-    head2->next->prev = head2;
-    head2->next->next = newNode(6);
-    head2->next->next->prev = head2->next;
-
-    // Merge the two lists
-    struct Node* mergedHead = mergeLists(head1, head2);
-
-    // Print the merged list
-    printList(mergedHead);
-
-    return 0;
+    doubly_FlistReader(head1);
+    printf("\n");
+    doubly_FlistReader(head2);
+    printf("\n");
+    Node output = mergeIt(head1, head2);
+    doubly_FlistReader(output);
 }
