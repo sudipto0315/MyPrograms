2d1a2cfe3bc50a7d8002b19be644d0eddda22fdf
sudipto0315 <sudiptoray2003@gmail.com>
2023-06-10 16:20:30 +0530

    fixes

--- VsCodePrograms/C++Programs/prog_1.cpp
+++ VsCodePrograms/C++Programs/prog_1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
+void forEachLoopChar()
+{
+  char vowels[5];
+  cout<<"Enter the vowels: "<<endl;
+  for (char &ele : vowels)//pass by reference
+  {
+    cin >> ele;
+  }
+  for (int i = 0; i < 5; i++)
+  {
+    cout << vowels[i];
+  }
+  cout<<"\n";
+}
 
-int main() {
-  cout << "Hello World!";
+void forEachLoopInteger(){
+  int arr[]={1,2,3,4,5};
+  for (int i = 0; i < 5; i++)
+  {
+    cout<<arr[i]<<endl;
+  }
+
+  for (int ele:arr)//pass by value
+  {
+    cout<<ele<<endl;
+  }
+  
+    
+}
+int main()
+{
+  forEachLoopChar();
+  forEachLoopInteger();
   return 0;
 }
