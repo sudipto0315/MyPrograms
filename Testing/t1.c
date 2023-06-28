bba60680bf6901d654af46cf416ea0ba2826c090
sudipto0315 <sudiptoray2003@gmail.com>
2023-06-29 00:05:13 +0530

    testing

--- Testing/t1.c
+++ Testing/t1.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void findDuplicates(int arr[], int size) {
+    // Create an array to store the frequency of each element
+    int freqArray[size];
+    for (int i = 0; i < size; i++) {
+        freqArray[i] = -1; // Initialize the frequency array with -1
+    }
+
+    // Calculate the frequency of each element in the input array
+    for (int i = 0; i < size; i++) {
+        int count = 1;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                // Mark the duplicate element as visited by setting its frequency to 0
+                freqArray[j] = 0;
+            }
+        }
+        if (freqArray[i] != 0) {
+            freqArray[i] = count;
+        }
+    }
+
+    // Print the duplicate elements along with their occurrences
+    printf("Duplicate numbers and their occurrences:\n");
+    for (int i = 0; i < size; i++) {
+        if (freqArray[i] > 1) {
+            printf("%d occurred %d times\n", arr[i], freqArray[i]);
+        }
+    }
+}
 
 int main() {
-  char *str = "Hello, world!";
-  str[0] = 'h'; // This line will cause a segmentation fault
-  return 0;
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findDuplicates(arr, size);
+
+    return 0;
 }
