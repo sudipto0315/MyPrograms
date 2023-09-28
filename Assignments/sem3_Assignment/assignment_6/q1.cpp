db8cb6f84bf191d663a0aefb389cbcdd5b621972
sudipto0315 <sudiptoray2003@gmail.com>
2023-09-28 17:08:41 +0530

    Assignment 6

---
+++ Assignments/sem3_Assignment/assignment_6/q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    if ((n == 0) || (n == 1))
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+int main()
+{
+    int n;
+    cout << "Enter a no. (1 <= n <= 50)";
+    cin >> n;
+    cout << "Factorial of " << n << " is " << fact(n);
+    return 0;
+}
