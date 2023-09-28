6a517c4c7942659ed71c0a4c0315e56ffaab51ee
sudipto0315 <sudiptoray2003@gmail.com>
2023-09-28 17:49:23 +0530

    changes

--- VsCodePrograms/Assignments/sem1_Assignment/assignment_8/random.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("%d",printf("Hello"));
-    return 0;
-}
-
--- VsCodePrograms/Assignments/sem1_Assignment/assignment_9/prog_10.c
+++
@@ -1,2 +0,0 @@
-// Write a program in C to dynamically allocate and deallocate memory for a lower
-// triangular matrix.
