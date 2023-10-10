f36b131afbe01733da030376fb5138ebb2aaad6f
sudipto0315 <sudiptoray2003@gmail.com>
2023-10-10 19:02:13 +0530

    Linked List

--- LinkedList/linkedListImplementation.cpp
+++ LinkedList/linkedListImplementation.cpp
@@ -2,42 +2,44 @@
 
 using namespace std;
 
-struct Node {
+typedef struct Node_t Node_t;
+typedef struct Node_t *Node;
+typedef Node List;
+
+struct Node_t {
     int data;
-    Node* next;
+    Node next;
 };
 
-typedef Node* List;
-
-Node* newNode(int data, Node* next) {
-    Node* node = new Node;
+Node newNode(int data, Node next) {
+    Node node = new Node_t;
     node->data = data;
     node->next = next;
     return node;
 }
 
-void addFirst(List* pnode, int data) {
+void addFirst(Node *pnode, int data) {
     *pnode = newNode(data, *pnode);
 }
 
-void addLast(List* pnode, int data) {
+void addLast(Node *pnode, int data) {
     if (*pnode == nullptr) {
         addFirst(pnode, data);
         return;
     }
-    Node* node = *pnode;
+    Node node = *pnode;
     while (node->next) {
         node = node->next;
     }
     node->next = newNode(data, nullptr);
 }
 
-int addAt(List* pnode, int data, unsigned int index) {
+int addAt(Node *pnode, int data, unsigned int index) {
     if (index == 0) {
         addFirst(pnode, data);
         return 1;
     }
-    Node* node = *pnode;
+    Node node = *pnode;
     index--;
     while (index > 0 && node != nullptr) {
         index--;
@@ -50,22 +52,22 @@ int addAt(List* pnode, int data, unsigned int index) {
     return 1;
 }
 
-int removeFirst(List* pnode) {
-    if (*pnode == nullptr) {
+int removeFirst(Node *pnode) {
+    if (*pnode == NULL) {
         return -1; // Return a sentinel value to indicate an error or empty list
     }
-    Node* node = *pnode;
+    Node node = *pnode;
     *pnode = (*pnode)->next;
     int data = node->data;
     delete node;
     return data;
 }
 
-int removeLast(List* pnode) {
+int removeLast(Node *pnode) {
     if (*pnode == nullptr || (*pnode)->next == nullptr) {
         return removeFirst(pnode);
     }
-    Node* node = *pnode;
+    Node node = *pnode;
     while (node->next->next) {
         node = node->next;
     }
@@ -75,27 +77,28 @@ int removeLast(List* pnode) {
     return data;
 }
 
-int removeFrom(List* pnode, unsigned int index) {
-    if (*pnode == nullptr || index == 0) {
+int removeFrom(Node *pnode, unsigned int index) {
+    if (*pnode == NULL || index == 0) {
         return removeFirst(pnode);
     }
-    Node* node = *pnode;
+    Node node = *pnode;
     index--;
     while (node && index) {
         node = node->next;
         index--;
     }
-    if (node == nullptr || node->next == nullptr) {
+    if (node == NULL || node->next == NULL) {
         return -1; // Return a sentinel value to indicate an error or invalid index
     }
-    Node* tempNode = node->next;
+    Node tempNode = node->next;
     node->next = tempNode->next;
     int data = tempNode->data;
     delete tempNode;
     return data;
 }
 
-void printList(Node* node) {
+void printList(Node node) {
+    cout<<"The Linked List is: ";
     while (node) {
         cout << node->data << " -> ";
         node = node->next;
@@ -104,24 +107,33 @@ void printList(Node* node) {
 }
 
 int main() {
-    Node* list = nullptr;
+    Node list = NULL;
     int data;
     int choice;
-
+    int n;
     // Initial user input to create the linked list
-    while (true) {
-        cout << "Enter an integer to add to the initial linked list (0 to finish): ";
-        cin >> data;
-        if (data == 0) {
-            break;
-        }
+    cout<<"Enter the no of nodes: ";
+    cin>>n;
+    cout<<"Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>data;
         addLast(&list, data);
     }
-
+    
     while (true) {
-        cout << "1. Add First\n2. Add Last\n3. Add At\n4. Remove From\n5. Print List\n6. Exit\nEnter your choice: ";
+        cout <<endl;
+        cout<<"1. Add First\n";
+        cout<<"2. Add Last\n";
+        cout<<"3. Add At\n";
+        cout<<"4. Remove First\n";
+        cout<<"5. Remove Last\n";
+        cout<<"6. Remove From\n";
+        cout<<"7. Print List\n";
+        cout<<"8. Exit\n";
+        cout<<"Enter your choice: ";
         cin >> choice;
-
+        cout<<endl;
         switch (choice) {
             case 1:
                 cout << "Enter data: ";
@@ -140,12 +152,18 @@ int main() {
                 cout << "Enter data: ";
                 cin >> data;
                 if (addAt(&list, data, index)) {
-                    cout << "Added successfully." << endl;
+                    cout << "The element " << data << " was Added successfully" << endl;
                 } else {
                     cout << "Invalid index." << endl;
                 }
                 break;
             case 4:
+                cout<<"The element "<<removeFirst(&list)<<" was removed !!"<<endl;
+                break;
+            case 5:
+                cout<<"The element "<<removeLast(&list)<<" was removed !!"<<endl;
+                break;
+            case 6:
                 unsigned int removeIndex;
                 cout << "Enter index to remove: ";
                 cin >> removeIndex;
@@ -156,10 +174,10 @@ int main() {
                     cout << "Invalid index." << endl;
                 }
                 break;
-            case 5:
+            case 7:
                 printList(list);
                 break;
-            case 6:
+            case 8:
                 // Exit the program
                 return 0;
             default:
