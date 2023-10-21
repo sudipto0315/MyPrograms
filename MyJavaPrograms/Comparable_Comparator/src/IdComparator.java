9243abccbffe2367981c185a50237821c38df9ed
sudipto0315 <sudiptoray2003@gmail.com>
2023-10-21 21:41:29 +0530

    changes

--- MyJavaPrograms/Comparable_Comparator/src/IdComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class IdComparator implements Comparator<Emp> {
-    @Override
-    public int compare(Emp o1, Emp o2) {
-        return o1.getEmpID() - o2.getEmpID();
-    }
-}
