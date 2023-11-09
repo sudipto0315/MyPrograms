cbbfe41bc408856c6f6ab42b8375546306ddea7d
sudipto0315 <sudiptoray2003@gmail.com>
2023-11-09 10:58:55 +0530

    Kruskal Algorithm

---
+++ Graph/KruskalAlgoWIthUnionByRank.cpp
@@ -0,0 +1,69 @@
+//with using union by rank
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Edge {
+    int u, v, weight;
+};
+
+bool cmp(Edge a, Edge b) {
+    return a.weight < b.weight;
+}
+
+int find(int x, vector<int>& parent) {
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x], parent); // recursively finds the parent node
+}
+
+void merge(int x, int y, vector<int>& parent, vector<int>& rank) {
+    x = find(x, parent);
+    y = find(y, parent);
+    if (x != y) {
+        if (rank[x] > rank[y]) {
+            parent[y] = x;
+        } else if (rank[x] < rank[y]) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+            rank[x]++;
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int n, m;
+    cout << "Enter number of vertices and edges: ";
+    cin >> n >> m;
+
+    vector<Edge> edges(m);
+    cout << "Enter edges in the format (u v w): ";
+    for (int i = 0; i < m; i++) {
+        cin >> edges[i].u >> edges[i].v >> edges[i].weight;
+    }
+
+    sort(edges.begin(), edges.end(), cmp);
+
+    vector<int> parent(n+1);
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
+
+    vector<int> rank(n+1, 0); // Initialize rank vector
+    vector<Edge> mst;
+    for (int i = 0; i < m; i++) {
+        int u = edges[i].u, v = edges[i].v, weight = edges[i].weight;
+        if (find(u, parent) != find(v, parent)) {
+            merge(u, v, parent, rank);
+            mst.push_back(edges[i]);
+        }
+    }
+
+    cout << "Minimum Spanning Tree:\n";
+    for (int i = 0; i < mst.size(); i++) {
+        cout << mst[i].u << " " << mst[i].v << " " << mst[i].weight << "\n";
+    }
+
+    return 0;
+}
