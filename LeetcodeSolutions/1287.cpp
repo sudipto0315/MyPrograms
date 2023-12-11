21122036a09a49b453f10011c2fd7154a69cb1b4
Sudipto Ray <sudiptoray2003@gmail.com>
2023-12-11 09:41:46 +0530

    DPP

---
+++ LeetcodeSolutions/1287.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                cnt++;
+                if (cnt > n / 4) return arr[i];
+            } else {
+                cnt = 1;
+            }
+        }
+        return arr[0];
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> arr1 = {1,2,2,6,6,6,6,7,10};
+    cout << sol.findSpecialInteger(arr1) << endl;
+    vector<int> arr2 = {1,1};
+    cout << sol.findSpecialInteger(arr2) << endl;
+    return 0;
+}
