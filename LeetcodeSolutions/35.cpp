16258f3333b77f1530eec02d19c9596225c12fe7
Sudipto Ray <sudiptoray2003@gmail.com>
2023-12-15 15:58:00 +0530

    Leetcode Solutions

---
+++ LeetcodeSolutions/35.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector> 
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if (nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else if(nums[mid]>target){
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+};  
+int main(){
+    Solution s;
+    vector<int> nums={1,3,5,6};
+    cout<<s.searchInsert(nums,5)<<endl;
+    cout<<s.searchInsert(nums,2)<<endl;
+    cout<<s.searchInsert(nums,7)<<endl;
+    return 0;
+}
---
+++ LeetcodeSolutions/88.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int c=0;
+        for(int i=m;i<nums1.size();i++){
+            nums1[i]=nums2[c];
+            c++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums1={1,2,3,0,0,0};
+    vector<int> nums2={2,5,6};
+    s.merge(nums1,3,nums2,3);
+    for(int i=0;i<nums1.size();i++){
+        cout<<nums1[i]<<" ";
+    }
+    return 0;
+}
