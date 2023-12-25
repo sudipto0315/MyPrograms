098730bca569ccc1266750c54e9f1fd0dd8678f8
Sudipto Ray <sudiptoray2003@gmail.com>
2023-12-25 21:34:47 +0530

    Leetcode Solutions

---
+++ LeetcodeSolutions/206.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Example usage
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    std::cout << "Original List: ";
+    printList(head);
+
+    Solution solution;
+    ListNode* reversedHead = solution.reverseList(head);
+
+    std::cout << "Reversed List: ";
+    printList(reversedHead);
+
+    // Clean up the memory
+    while (reversedHead != nullptr) {
+        ListNode* temp = reversedHead;
+        reversedHead = reversedHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
