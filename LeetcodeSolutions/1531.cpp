79d1ae7ba85444b87293315e4a7a3cf29cfaf0f6
Sudipto Ray <sudiptoray2003@gmail.com>
2023-12-29 08:55:30 +0530

    DPP

---
+++ LeetcodeSolutions/1531.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+int dp[101][27][101][101];
+class Solution {
+public:
+    unordered_set<int>possibleLen={1,9,99};
+    int solve(string& s,int idx,char lastChar,int lastCharCount, int k){
+        if(k<0)return INT_MAX/4;
+        if(idx==s.length())return 0;
+        int& ans=dp[idx][lastChar-'a'][lastCharCount][k];
+        if(ans!=-1)return ans;
+        int keepChar;
+        int deleteChar=solve(s,idx+1,lastChar,lastCharCount,k-1);
+        if(s[idx]==lastChar){
+            keepChar=solve(s,idx+1,lastChar,lastCharCount+1,k)+(possibleLen.count(lastCharCount)?1:0);
+        }else{
+            keepChar=solve(s,idx+1,s[idx],1,k)+1;
+        }
+        ans=min(keepChar,deleteChar);
+        return ans;
+    }
+    int getLengthOfOptimalCompression(string s, int k) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0,'z'+1,0,k);
+    }
+};
+int main(){
+    Solution s;
+    cout<<s.getLengthOfOptimalCompression("aaabcccd",2)<<endl;
+    cout<<s.getLengthOfOptimalCompression("aabbaa",2)<<endl;
+    cout<<s.getLengthOfOptimalCompression("aaaaaaaaaaa",0)<<endl;
+    return 0;
+}
