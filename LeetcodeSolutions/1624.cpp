82cc684b0b052cde826ddaa8537165346d6658e2
Sudipto Ray <sudiptoray2003@gmail.com>
2023-12-31 16:55:55 +0530

    DPP

---
+++ LeetcodeSolutions/1624.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int length=-1;
+        int n=s.length();
+        unordered_map<char,int> hash;
+        for(int i=0;i<n;i++){
+            if(hash.find(s[i])!=hash.end()){
+                length=max(length,i-hash[s[i]]-1);
+            }else{
+                hash[s[i]]=i;
+            }
+        }
+        return length;
+        cout <<"udhnvidnvi"<<hash.end()<<endl;
+    }
+};
+int main(){
+    Solution s;
+    cout<<s.maxLengthBetweenEqualCharacters("aa")<<endl;
+    // cout<<s.maxLengthBetweenEqualCharacters("abca")<<endl;
+    // cout<<s.maxLengthBetweenEqualCharacters("cbzxy")<<endl;
+    return 0;
+}
